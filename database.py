09dc387a35c6095e4d9e9b9f34f5d5b8fcdc62eb
Rob Hay <robhay@outlook.com>
2025-04-26 15:07:24 +0100

    Fix database connection to use Render environment variable

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 # database.py
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres.xfvqaitclnpmzaiimdst: jejbeh-toqzoR-2xaxwi @aws-0-eu-west-2.pooler.supabase.com:6543/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")  # Supabase connection string from Render environment
 
 engine = create_engine(DATABASE_URL, connect_args={}, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
