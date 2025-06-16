96c3b2e661e8eeeffa11948dec57b6f856447c5d
alpha2791 <robhay@outlook.com>
2025-06-16 21:26:43 +0100

    Add root route to confirm API is running

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+@app.get("/")
+def root():
+    return {"message": "Turf Tracker Irrigation Forecast API is running"}
 
 
 MODEL_FILE = "moisture_model.pkl"
